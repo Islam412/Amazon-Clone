48af5371735e7dbe181f08e623dc3ead0eeaf1be
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-15 21:53:48 +0200

    desing ProductDetails views

--- products/views.py
+++ products/views.py
@@ -46,3 +46,17 @@ class ProductList(ListView):
     model = Product
     paginate_by = 30
     ordering = ['-id']
+
+
+
+class ProductDetails(DetailView):
+    model = Product
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.get_object()
+        context["reviews"] = Review.objects.filter(product=product)
+        context["average_rating"] = product.average_rating
+        context["rate_products"] = Product.objects.filter(brand=product.brand)
+        return context
+    
