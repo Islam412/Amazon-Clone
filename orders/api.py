77107c70b0becd2338e0001c0594c26ddbbbbe0a
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-15 22:51:23 +0200

    desing CartDetailCreateAPI

---
+++ orders/api.py
@@ -0,0 +1,51 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
+from rest_framework.response import Response
+
+import datetime
+
+from .serializers import CartDetailSerializer , CartSerializer , OrderListSerializer , OrderDetailsSerializer
+from .models import Cart , CartDetails , Order, OrderDetails ,Coupon
+from products.models import Product
+from userauths.models import User
+
+
+
+class CartDetailCreateAPI(generics.GenericAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [AllowAny]
+
+    def get(self,request,*args,**kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        cart,created = Cart.objects.get_or_create(user=user,status='InProgress')
+        data = CartSerializer(cart).data
+        return Response({'cart':data})
+    def post(self, request, *args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        product = Product.objects.get(id=request.data['product_id'])
+        quantity = int(request.data['quantity'])
+        cart = Cart.objects.get(user=user,status='InProgress')
+        cart_detail , creeate = CartDetails.objects.get_or_create(cart=cart, product=product)
+        cart_detail.quantity = int(quantity)
+        cart_detail.total = round(quantity * product.price , 2)
+        cart_detail.save()
+
+        cart = Cart.objects.get(user=user,status='InProgress')
+        data = CartSerializer(cart).data
+        return Response({'message':'Cart item added successfully' ,'cart':data})
+
+    def delete(self, request, *args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        cart_detail =CartDetails.objects.get(id=request.data['cart_detail_id'])
+        cart_detail.delete()
+
+        cart = Cart.objects.get(user=user,status='InProgress')
+        data = CartSerializer(cart).data
+        return Response({'message':'Cart product deleted successfully' ,'cart':data})
+    
+
+
+
+
