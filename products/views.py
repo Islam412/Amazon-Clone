4eec689f1492e1b8686466da48c9a6a9782cb82e
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-15 21:54:37 +0200

    desing BrandList

--- products/views.py
+++ products/views.py
@@ -60,3 +60,10 @@ class ProductDetails(DetailView):
         context["rate_products"] = Product.objects.filter(brand=product.brand)
         return context
     
+
+
+class BrandList(ListView):
+    model = Brand
+    queryset = Brand.objects.annotate(products_count=Count('product_name'))
+    paginate_by = 20
+    ordering = ['-id']
