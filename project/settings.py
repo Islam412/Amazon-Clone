8aa8c84201abca54bd2a2f7726e368537142a936
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-15 14:21:02 +0200

    update reqierment file

--- project/settings.py
+++ project/settings.py
@@ -66,22 +66,19 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "django.middleware.locale.LocaleMiddleware", # django translate middelware
+    'django.middleware.locale.LocaleMiddleware',  # Django translation middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-
-    # custom with auths
-    # 'allauth.account.auth_backends.AuthenticationBackend',
-
-    # debug_toolbar library
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'allauth.account.middleware.AccountMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',  # Debug toolbar middleware
+    'allauth.account.middleware.AccountMiddleware',  # Custom middleware for Django Allauth
 ]
+
+
+
+
 
 
 INTERNAL_IPS = [
