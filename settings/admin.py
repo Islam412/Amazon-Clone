c2343ac95420af30acfb999a67643601828a3e38
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-18 17:20:31 +0200

    call fee offer models at admin

--- settings/admin.py
+++ settings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Company , DeliveryFee
+from .models import Company , DeliveryFee , FeeOffer
 
 # Register your models here.
 
 admin.site.register(Company)
 admin.site.register(DeliveryFee)
+admin.site.register(FeeOffer)
