f408ee1895523ad6c173c6379675bbb154aea0f1
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-18 17:52:15 +0200

    create urls free offer list views

--- settings/urls.py
+++ settings/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home , contact , need_help
+from .views import home , contact , need_help , free_offer_list
 
 app_name = 'settings'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('contact/', contact, name='contact'),
     path('faq/', need_help, name='faq'),
+    path('offers/', free_offer_list, name='free_offer_list'),
 ]
