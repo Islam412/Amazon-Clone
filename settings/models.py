24c939faefe5ba99a7831045d11222a884c02a81
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-18 20:48:20 +0200

    add new migrate

--- settings/models.py
+++ settings/models.py
@@ -30,7 +30,7 @@ class Company(models.Model):
 
 
 class DeliveryFee(models.Model):
-    fee = models.FloatField(null=True, blank=True)
+    fee = models.FloatField(default=50.0)
     def __str__(self):
         return str(self.fee)
 
