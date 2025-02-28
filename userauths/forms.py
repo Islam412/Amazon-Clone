78dc459c77624d66752863105279fdf664a653cd
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-28 02:35:51 +0200

    desing SetNewPasswordForm

--- userauths/forms.py
+++ userauths/forms.py
@@ -173,3 +173,20 @@ class PasswordResetRequestForm(forms.Form):
         if not User.objects.filter(email=email).exists():
             raise forms.ValidationError("No user found with this email.")
         return email
+
+
+
+
+class SetNewPasswordForm(forms.Form):
+    new_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'New Password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match!")
+
+        return cleaned_data
